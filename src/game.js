f85dd0f43b97fcbe87a4fc9ff6852de6c7dec203
foxyflow <foxyflow@contractor.net>
2023-01-19 00:21:31 +1300

    more tilemaps added

--- src/game.js
+++ src/game.js
@@ -7,7 +7,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: true,
+            debug: false,
             gravity: { y: 0 } // Top down game, so no gravity.
         }
     },
@@ -25,12 +25,13 @@ function preload (){
     this.load.image('tiles', 'src/assets/dungeon.png'); //dungeon tileset (1)
     this.load.image('tileset', 'src/assets/tileset.png'); // fox tileset (2)
     this.load.image('castle_tileset', 'src/assets/castle_tileset.png'); // castle tileset (3
+    this.load.image('tileset_grassland', 'src/assets/tileset_grassland.png'); // grassland tileset (4)
+    this.load.image('tilesetdeep', 'src/assets/tilesetdeep.png'); // deep tileset (5)
+    this.load.image('tilesetpurple', 'src/assets/tilesetpurple.png'); // purple tileset (6)
+    this.load.image('coolcastle', 'src/assets/coolcastle-tileset.png'); // cool castle tileset (7)
     this.load.tilemapTiledJSON('map1', 'src/assets/map1.json');
     //Player just as image:
-        // this.load.image('player', 'src/assets/player-crouch-1-16x15.png');
     this.load.spritesheet('player', 'src/assets/player-run-184x22.png', { frameWidth: 30, frameHeight: 22 });
-
-
 }
 let player;
 let cursors;
@@ -40,11 +41,15 @@ function create (){
     const tileset = map.addTilesetImage('tiled1', 'tiles');
     const tileset2 = map.addTilesetImage('tiled2', 'tileset');
     const tileset3 = map.addTilesetImage('tiled3', 'castle_tileset');
-    const belowLayer = map.createStaticLayer("Below Player", [tileset, tileset2, tileset3], 0, 0);
-    const worldLayer = map.createStaticLayer("World",[tileset, tileset2, tileset3], 0, 0);
-    const aboveLayer = map.createStaticLayer("Above Player", [tileset, tileset2, tileset3], 0, 0);
+    const tileset4 = map.addTilesetImage('tiled4', 'tileset_grassland');
+    const tileset5 = map.addTilesetImage('tiled5', 'tilesetdeep');
+    const tileset6 = map.addTilesetImage('tiled6', 'tilesetpurple');
+    const tileset7 = map.addTilesetImage('tiled7', 'coolcastle');
+    const belowLayer = map.createStaticLayer("Below Player", [tileset, tileset2, tileset3, tileset4, tileset5, tileset6, tileset7], 0, 0);
+    const worldLayer = map.createStaticLayer("World",[tileset, tileset2, tileset3, tileset4, tileset5, tileset6, tileset7], 0, 0);
+    const aboveLayer = map.createStaticLayer("Above Player", [tileset, tileset2, tileset3, tileset4, tileset5, tileset6, tileset7], 0, 0);
     
-    //Create player without Tiled: 
+    //Create player without Tiled Object Spawn Point: 
         //player = this.physics.add.sprite(100, 500, 'player');
     const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
     player = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, "player");
@@ -53,7 +58,7 @@ function create (){
     player.setCollideWorldBounds(true);
     this.physics.add.collider(player, worldLayer);
     worldLayer.setCollisionByProperty({collides: true});
-    player.body.setSize(player.width - 10, player.height - 10, true); //true centers and the rest changes collisionbox size
+    player.body.setSize(player.width - 20, player.height - 10, true); //true centers and the rest changes collisionbox size
 
     this.anims.create({
         key: 'left',
@@ -69,13 +74,20 @@ function create (){
         frameRate: 10,
         repeat: -1
     });
+    this.anims.create({ //not active yet. -- Add Sprite
+        key: 'down',
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 1 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
     cursors = this.input.keyboard.createCursorKeys();
 
 }// End of create
 
-const speed = 161;
+const speed = 150;
 function update (){
-    // game logic: updates every frame
+// game logic: updates every frame
     
     if (cursors.left.isDown )
     {
@@ -83,13 +95,14 @@ function update (){
         player.setVelocityX(-speed);
         player.anims.play('left', true);
     }
-    else if (cursors.up.isDown){
-       
-        player.setVelocityY(-speed);
-    }
     else if (cursors.down.isDown){
-        
+       
         player.setVelocityY(speed);
+        player.anims.play('down', true);//not active yet.
+    }
+    else if (cursors.up.isDown){
+        
+        player.setVelocityY(-speed);
     }
     else if (cursors.right.isDown)
     {
